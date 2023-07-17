1952b8c675d90c05fd949608989a7ba7b361bb61
guillermouzner <guillermouz16@gmail.com>
2023-07-16 23:16:18 -0300

    x

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -1,26 +1,60 @@
 import * as React from "react";
+import Image from "next/image";
 
 import {cn} from "@/lib/utils";
-import {Icons} from "@/components/icons";
 import {ModeToggle} from "@/components/mode-toggle";
 
 export function SiteFooter({className}: React.HTMLAttributes<HTMLElement>) {
   return (
-    <footer className={cn(className)}>
-      <div className="container flex items-center justify-between gap-4 md:h-24 flex-row py-2">
-        <div className="flex items-center gap-4 px-2 flex-row md:gap-2 md:px-0">
-          <Icons.logo />
-          <p className="text-center text-sm leading-loose md:text-left">
-            Built by{" "}
+    <footer className={cn(className, "py-4")}>
+      <div className="container flex items-center justify-between gap-4 md:h-24 flex-col lg:flex-row py-2">
+        <div className="flex flex-row items-center gap-2">
+          <Image
+            alt="Milton"
+            className="hidden lg:block"
+            height={75}
+            src="/carbobasicfooter.png"
+            width={75}
+          />
+
+          <div className="flex flex-row gap-1 lg:flex-col font-bold text-lg md:text-xl">
+            <p className="uppercase text-blue-900 text-lg md:text-2xl">
+              carbo<span className="text-green-600">Basic</span>
+            </p>
+            <p className="uppercase">engineering Ltd.</p>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-3 font-medium">
+          <p className="capitalize text-center">
+            for any inquiries please contact on our social networks:
+          </p>
+          <div className="flex flex-row gap-8 items-center justify-center">
             <a
-              className="font-medium underline underline-offset-4"
-              href={"https://www.instagram.com/wau.tuc/"}
+              className="hover:underline"
+              href="mailto:info@carbobasic.co.uk"
               rel="noreferrer"
               target="_blank"
             >
-              Wau!
+              email
             </a>
-          </p>
+            <a
+              className="hover:underline"
+              href="https://www.facebook.com/Carbobasic/"
+              rel="noreferrer"
+              target="_blank"
+            >
+              facebook
+            </a>
+            <a
+              className="hover:underline"
+              href="https://wa.me/447438389980"
+              rel="noreferrer"
+              target="_blank"
+            >
+              whatsapp
+            </a>
+          </div>
         </div>
         <ModeToggle />
       </div>
